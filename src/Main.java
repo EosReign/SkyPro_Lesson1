d10d366607e4d7ce2295358bbf029b956de95a40
EosReign <107928418+EosReign@users.noreply.github.com>
2022-08-18 12:43:17 +0300

    Branch Request

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("How do you do Fellow Programmers?");
-        System.out.println("Nah~");
+        System.out.println("Nah...");
     }
 }
